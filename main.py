473702aade4d44e12d8b1256fce8257bdcc206be
94104820+HKSSY@users.noreply.github.com <94104820+HKSSY@users.noreply.github.com>
2023-04-21 05:39:11 +0800

    Version 0.9.6

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import socket
 import pandas
 
 from pathlib import Path
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask import request
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType, BooleanType, TimestampType
 from pyspark.sql.functions import count, countDistinct, col, max as sparkMax
+from pyspark.sql.functions import monotonically_increasing_id
+
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -23,7 +25,7 @@ print('  \:\  \            \::/  /   \:\ \:\__\    \:\ \:\__\     /:/  /        
 print('   \:\  \           /:/  /     \:\ \/__/     \:\ \/__/     \/__/            /:/  /        /:/  /   ')
 print('    \:\__\         /:/  /       \:\__\        \:\__\                       /:/  /        /:/  /    ')
 print('     \/__/         \/__/         \/__/         \/__/                       \/__/         \/__/     ')
-print('                                COMP4442 Project     Version: 0.9.5                                ')
+print('                                COMP4442 Project     Version: 0.9.6                                ')
 
 # Check whether the port is open. if it is used by other application, it will switch the other listen port
 host = "localhost"
@@ -52,7 +54,7 @@ def load_dataset():
     StructField("Speed", IntegerType(), True),
     StructField("Direction", StringType(), True),
     StructField("siteName", StringType(), True),
-    StructField("Time", StringType(), True),
+    StructField("Time", TimestampType(), True),
     StructField("isRapidlySpeedup", StringType(), True),
     StructField("isRapidlySlowdown", StringType(), True),
     StructField("isNeutralSlide", StringType(), True),
@@ -81,6 +83,7 @@ def load_dataset():
                 .option('escape', '"')\
                 .schema(customSchema)\
                 .csv(dataset_full_path_list)
+        dataset_dataframe = dataset_dataframe.withColumn('id', monotonically_increasing_id())
         dataset_dataframe.createOrReplaceTempView("RECORD")
         dataset_dataframe.cache()
 
@@ -96,8 +99,80 @@ def load_dataset():
 
 app = Flask(__name__, static_url_path='/static', template_folder='static/') # add path for the HTML files
 
-@app.route("/", methods=['GET'])
+
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/fetch_data', methods=['POST'])
+def fetch_data():
+    start = int(request.form['start'])
+    length = int(request.form['length'])
+    draw = int(request.form['draw'])
+    search_value = request.form['search[value]']
+    
+    # Add search condition
+    search_condition = ""
+    if search_value:
+        search_condition = f"""WHERE (
+        LOWER(driverID) LIKE LOWER('%{search_value}%') OR
+        LOWER(carPlateNumber) LIKE LOWER('%{search_value}%') OR
+        LOWER(Latitude) LIKE LOWER('%{search_value}%') OR
+        LOWER(Longtitude) LIKE LOWER('%{search_value}%') OR
+        LOWER(Speed) LIKE LOWER('%{search_value}%') OR
+        LOWER(siteName) LIKE LOWER('%{search_value}%') OR
+        LOWER(Time) LIKE LOWER('%{search_value}%')
+        )"""
+
+    # Get the ordering information from the request
+    order_column = request.form['order[0][column]']
+    order_dir = request.form['order[0][dir]']
+    order_column_name = request.form[f'columns[{order_column}][data]']
+
+    records_total = spark.sql("SELECT COUNT(*) as count FROM RECORD").collect()[0]["count"]
+    records_filtered = records_total
+
+    sql_results = spark.sql(f"""
+        SELECT * FROM (
+            SELECT *, ROW_NUMBER() OVER(ORDER BY {order_column_name} {order_dir}) as row_num FROM RECORD {search_condition}
+        ) tmp
+        WHERE tmp.row_num > {start} AND tmp.row_num <= {start + length}
+    """).collect()
+
+    data = [{
+        "id": sql_result["id"],
+        "driverID": sql_result["driverID"],
+        "carPlateNumber": sql_result["carPlateNumber"],
+        "Latitude": sql_result["Latitude"],
+        "Longtitude": sql_result["Longtitude"],
+        "Speed": sql_result["Speed"],
+        "siteName": sql_result["siteName"],
+        "Time": sql_result["Time"],
+        "isRapidlySpeedup": sql_result["isRapidlySpeedup"],
+        "isRapidlySlowdown": sql_result["isRapidlySlowdown"],
+        "isNeutralSlide": sql_result["isNeutralSlide"],
+        "isNeutralSlideFinished": sql_result["isNeutralSlideFinished"],
+        "neutralSlideTime": sql_result["neutralSlideTime"],
+        "isRapidlySlowdown": sql_result["isRapidlySlowdown"],
+        "isOverspeed": sql_result["isOverspeed"],
+        "isOverspeedFinished": sql_result["isOverspeedFinished"],
+        "overspeedTime": sql_result["overspeedTime"],
+        "isFatigueDriving": sql_result["isFatigueDriving"],
+        "isHthrottleStop": sql_result["isHthrottleStop"],
+        "isOilLeak": sql_result["overspeedTime"]
+    } for sql_result in sql_results]
+
+    response = {
+        "draw": draw,
+        "recordsTotal": records_total,
+        "recordsFiltered": records_filtered,
+        "data": data
+    }
+
+    return jsonify(response)
+
+@app.route("/summary", methods=['GET'])
+def summary():
     sql_query = "driverID AS `Driver ID`, ROUND(AVG(Speed), 1) AS `AVG Speed`, COUNT(isRapidlySpeedup) AS `Rapidly Speedup`, \
     COUNT(isRapidlySlowdown) AS `Rapidly Slowdown`, COUNT(isNeutralSlide) AS `Neutral Slide`, SUM(neutralSlideTime) AS `Total Neutral Slide`, \
     COUNT(isOverspeed) AS `Overspeed`, SUM(overspeedTime) AS `Total Overspeed`, COUNT(isFatigueDriving) AS `Fatigue Driving`, \
@@ -106,10 +181,10 @@ def index():
     output_summary = spark.sql("SELECT " + sql_query + " FROM RECORD " + sql_groupby_query).toPandas()
     response = output_summary
     #print(response)
-    return render_template('index.html', tables=[response.to_html(classes='g--12 g-s--12 card', index=False).replace('<tr style="text-align: right;">', '<tr class="table-header">')], titles=response.columns.values)
+    return render_template('summary.html', tables=[response.to_html(classes='g--12 g-s--12 card', index=False).replace('<tr style="text-align: right;">', '<tr class="table-header">')], titles=response.columns.values)
     #return app.send_static_file('index.html')
 
-@app.route("/test", methods=['GET', 'POST'])
+@app.route("/texxst", methods=['GET', 'POST'])
 def sparkpi():
     #test = dataset_dataframe.select('driverID').collect()
     #test = dataset_dataframe.where("Time = '2017-01-01'").collect()
@@ -128,17 +203,17 @@ def sparkpi():
     sql_query = "driverID"
     sql_groupby_query = "GROUP BY driverID"
     output_driver_id = spark.sql("SELECT " + sql_query + " FROM RECORD " + sql_groupby_query).rdd.map(lambda x : x[0]).collect()
-    print(output_driver_id)
+    #print(output_driver_id)
     sql_query = "carPlateNumber"
     sql_groupby_query = "GROUP BY carPlateNumber"
     output_car_plate_number = spark.sql("SELECT " + sql_query + " FROM RECORD " + sql_groupby_query).rdd.map(lambda x : x[0]).collect()
-    print(output_car_plate_number)
+    #print(output_car_plate_number)
     response = output_driver_id
     #print(response)
     return render_template('test.html', option_driver_id=output_driver_id, opt_car_plate_number=output_car_plate_number)
 
 @app.route("/about", methods=['GET'])
-def test():
+def about_page():
     return app.send_static_file('about.html')
 
 @app.errorhandler(500)
@@ -151,4 +226,4 @@ def handle_bad_request(e):
 
 if __name__ == "__main__":
     load_dataset()
-    app.run(host, port)
+    app.run(host, port, debug=True)
